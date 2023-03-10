e44eb26d0cff8c7f3f6b8765cf3e851f00a7db6f
James <james.d.langridge@gmail.com>
2023-03-10 13:55:04 +0100

    Handle route not found errors

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,9 +1,15 @@
 import {ComponentResolver} from '@/components/ComponentResolver'
 import {getPageData} from '@/lib/contentful'
+import {notFound} from 'next/navigation'
 
 export default async function Page({params}: {params: {slug: string}}) {
   const {slug} = params
   const pageData = await getPageData(slug)
+
+  if (!pageData.items.length) {
+    notFound()
+  }
+
   const pageContent = pageData.items[0].fields.pageContent
 
   return (
