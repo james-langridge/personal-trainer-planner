76f3eeebbe10ac2d5e7430a319d5eec2db338aa9
James <james.d.langridge@gmail.com>
2023-03-10 15:24:25 +0100

    Link logo to homepage

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,13 +9,15 @@ function Navbar({entry}: {entry: Entry<INavbarFields>}) {
   return (
     <div className="fixed top-0 z-50 flex w-full flex-wrap justify-end gap-5 bg-slate-100 p-5">
       <div className="grow">
-        <Image
-          src={`https:${logo.fields.file.url}`}
-          alt={logo.fields.title}
-          width={logo.fields.file.details.image?.width}
-          height={logo.fields.file.details.image?.height}
-          className="max-h-10 w-auto"
-        />
+        <Link href="/">
+          <Image
+            src={`https:${logo.fields.file.url}`}
+            alt={logo.fields.title}
+            width={logo.fields.file.details.image?.width}
+            height={logo.fields.file.details.image?.height}
+            className="max-h-10 w-auto"
+          />
+        </Link>
       </div>
       {navbarItems?.map(item => (
         <Link key={item.sys.id} href={item.fields.link}>
