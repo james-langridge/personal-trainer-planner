3e9ade38837674c531270e5955cc98409d653662
James <james.d.langridge@gmail.com>
2023-03-17 15:59:33 +0100

    Improve logic in ComponentRenderer Removed unecessary use of React.lazy, and moved component map into renderer file for readability and maintainability.

--- components/ComponentRenderer.tsx
+++ components/ComponentRenderer.tsx
@@ -1,26 +1,41 @@
 import {IEntry} from '@/@types/generated/contentful'
-import {componentMap} from '@/components/mappings'
 import React from 'react'
 import {Entry} from 'contentful'
+import Image from '@/components/Image'
+import ProgramAd from '@/components/ProgramAd'
+import ProgramAdBanner from '@/components/ProgramAdBanner'
+import RichTextComponent from '@/components/RichTextComponent'
+import Testimonial from '@/components/Testimonial'
+import VideoHero from '@/components/VideoHero'
+
+const componentMap: {[key: string]: React.ComponentType<any>} = {
+  image: Image,
+  programAd: ProgramAd,
+  programAdBanner: ProgramAdBanner,
+  richText: RichTextComponent,
+  testimonial: Testimonial,
+  videoHeroFeature: VideoHero,
+}
 
 export const ComponentRenderer: React.FC<{entry: Entry<IEntry>}> = ({
   entry,
 }) => {
   const {contentType} = entry.sys
   const entryType = contentType.sys.id
-  const Component = componentMap[entryType] as React.ComponentType<any>
-  const {fields} = entry
-  let childrenEntries: Entry<IEntry>[] = []
-
-  for (const [_, value] of Object.entries(fields)) {
-    if (Array.isArray(value)) {
-      childrenEntries = value
-      break
-    }
-  }
+  const Component = componentMap[entryType]
 
   if (!Component) {
     return null
+  }
+
+  const {fields} = entry
+  let childrenEntries: Entry<IEntry>[] = []
+
+  for (const field of Object.values(fields)) {
+    if (Array.isArray(field)) {
+      childrenEntries = field
+      break
+    }
   }
 
   return (
