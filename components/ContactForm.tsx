a7b553374ae4c00ec7534ff31aa83e553b2b2f5d
James <james.d.langridge@gmail.com>
2023-03-21 10:37:20 +0100

    Add sending status alert to contact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -3,13 +3,62 @@
 import React, {useState} from 'react'
 import {submitContactForm} from '@/lib/api'
 
-const initialState = {name: '', email: '', message: ''}
+function Info({status, error}: {status: string; error: Error | null}) {
+  if (status === 'idle') {
+    return null
+  }
+
+  if (status === 'pending') {
+    return (
+      <div
+        role="alert"
+        className="mt-4 rounded bg-yellow-600 p-2.5 text-center text-white"
+      >
+        Sending message...
+      </div>
+    )
+  }
+
+  if (status === 'rejected') {
+    return (
+      <div
+        role="alert"
+        className="mt-4 rounded bg-red-700 p-2.5 text-center text-white"
+      >
+        Error sending message:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error?.message}</pre>
+      </div>
+    )
+  }
+
+  if (status === 'resolved') {
+    return (
+      <div
+        role="alert"
+        className="mt-4 rounded bg-green-700 p-2.5 text-center text-white"
+      >
+        Thank you for your message. I&apos;ll get back to you in a jiffy!
+      </div>
+    )
+  }
+
+  return null
+}
+
+const initialState = {
+  status: 'idle',
+  form: {name: '', email: '', message: ''},
+  error: null,
+}
 
 export default function ContactForm() {
   const [state, setState] = useState(initialState)
+  const {status, form, error} = state
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault()
+
+    setState({...state, status: 'pending'})
 
     // Previously, API Routes could have been used for use cases like
     // handling form submissions. Route Handlers are likely not the solution
@@ -17,9 +66,18 @@ export default function ContactForm() {
     // this when ready.
     // https://beta.nextjs.org/docs/routing/route-handlers#dynamic-route-handlers
     // https://beta.nextjs.org/docs/data-fetching/mutating
-    await submitContactForm(state).then(
-      () => setState(initialState),
-      error => console.error(error),
+    await submitContactForm(form).then(
+      () =>
+        setState({
+          ...initialState,
+          status: 'resolved',
+        }),
+      error =>
+        setState({
+          ...initialState,
+          status: 'rejected',
+          error,
+        }),
     )
   }
 
@@ -30,9 +88,14 @@ export default function ContactForm() {
           <label className="mb-2 block text-sm text-gray-600 dark:text-gray-200">
             Full Name
             <input
-              onChange={e => setState({...state, name: e.target.value})}
+              onChange={e =>
+                setState({
+                  ...state,
+                  form: {...state.form, name: e.target.value},
+                })
+              }
               required
-              value={state.name}
+              value={form.name}
               type="text"
               placeholder="John Doe"
               className="mt-2 block w-full rounded-md border border-gray-200 bg-white px-5 py-3 text-gray-700 placeholder-gray-400 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:placeholder-gray-600 dark:focus:border-blue-400"
@@ -44,11 +107,16 @@ export default function ContactForm() {
           <label className="mb-2 block text-sm text-gray-600 dark:text-gray-200">
             Email address
             <input
-              onChange={e => setState({...state, email: e.target.value})}
+              onChange={e =>
+                setState({
+                  ...state,
+                  form: {...state.form, email: e.target.value},
+                })
+              }
               type="email"
               placeholder="johndoe@example.com"
               required
-              value={state.email}
+              value={form.email}
               className="mt-2 block w-full rounded-md border border-gray-200 bg-white px-5 py-3 text-gray-700 placeholder-gray-400 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:placeholder-gray-600 dark:focus:border-blue-400"
             />
           </label>
@@ -60,13 +128,20 @@ export default function ContactForm() {
           Message
           <textarea
             required
-            value={state.message}
-            onChange={e => setState({...state, message: e.target.value})}
+            value={form.message}
+            onChange={e =>
+              setState({
+                ...state,
+                form: {...state.form, message: e.target.value},
+              })
+            }
             className="mt-2 block h-32 w-full rounded-md border border-gray-200 bg-white px-5 py-3 text-gray-700 placeholder-gray-400 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:placeholder-gray-600 dark:focus:border-blue-400 md:h-56"
             placeholder="Message"
           ></textarea>
         </label>
       </div>
+
+      <Info status={status} error={error} />
 
       <button
         type="submit"
