3f15853a8694e4a9fdbd0e2be5c4eea7817565e8
James <james.d.langridge@gmail.com>
2023-06-21 15:47:58 +0200

    Improve useUserWorkouts Moved logic to set initial user from Calendar component into this hook. Restricted refresh of user workouts to admin user, as it is only needed when selecting a user in admin dashboard. Previously, the refresh was immediately triggered when the user was set. This fixes https://github.com/james-langridge/personal-trainer-planner/issues/17

--- hooks/useUserWorkouts.ts
+++ hooks/useUserWorkouts.ts
@@ -1,13 +1,20 @@
-import {getUserWithWorkouts} from '@/lib/api'
 import {useCallback, useEffect} from 'react'
+
 import {useUser, useUserDispatch} from '@/app/Providers'
+import {getUserWithWorkouts} from '@/lib/api'
+import {SerialisedUser} from '@/lib/users'
 
 type UseUserWorkoutsReturnType = [() => Promise<void>]
 
-export function useUserWorkouts(): UseUserWorkoutsReturnType {
+export function useUserWorkouts({
+  initialUser,
+}: {
+  initialUser?: SerialisedUser
+} = {}): UseUserWorkoutsReturnType {
   const dispatchUser = useUserDispatch()
-  const userState = useUser()
-  const userId = userState?.user?.id ?? ''
+  const {user} = useUser()
+  const userId = user?.id
+  const isAdmin = user?.role === 'admin'
 
   const refreshUserWithWorkouts = useCallback(async () => {
     const user = await getUserWithWorkouts(userId)
@@ -15,11 +22,23 @@ export function useUserWorkouts(): UseUserWorkoutsReturnType {
     dispatchUser({type: 'setUser', user: user})
   }, [dispatchUser, userId])
 
+  // Admin starts with blank calendar, and manually selects user
   useEffect(() => {
+    if (initialUser && initialUser.role !== 'admin') {
+      dispatchUser({type: 'setUser', user: initialUser})
+    }
+  }, [dispatchUser, initialUser])
+
+  // Only relevant to admin when changing user
+  useEffect(() => {
+    if (!isAdmin) {
+      return
+    }
+
     if (userId) {
       void refreshUserWithWorkouts()
     }
-  }, [refreshUserWithWorkouts, userId])
+  }, [isAdmin, refreshUserWithWorkouts, userId])
 
   // This is to sync updates between users (personal trainer and their clients)
   useEffect(() => {
