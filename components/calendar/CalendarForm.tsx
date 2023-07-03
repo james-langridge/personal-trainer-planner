7598cb596f8560136a6ab693249727464c8a0a5f
James <james.d.langridge@gmail.com>
2023-07-03 17:48:29 +0200

    Create UI for repeating days option

--- components/calendar/CalendarForm.tsx
+++ components/calendar/CalendarForm.tsx
@@ -1,4 +1,5 @@
 import {WORKOUT_TYPE} from '@prisma/client'
+import clsx from 'clsx'
 import Link from 'next/link'
 import React, {useEffect, useState} from 'react'
 
@@ -14,16 +15,18 @@ import {useAppDispatch, useAppSelector} from '@/redux/hooks'
 import {resetWorkoutId} from '@/redux/workoutSlice'
 
 export function CalendarForm({
-  date,
+  day,
   closeModal,
 }: {
-  date: string
+  day: Day
   closeModal: (e: React.SyntheticEvent) => void
 }) {
   const user = useAppSelector(state => state.users.user)
   const dispatch = useAppDispatch()
   const userId = user?.id
-  const [workout, setWorkout, resetForm] = useCalendarForm({date})
+  const [workout, setWorkout, toggleDay] = useCalendarForm({
+    day,
+  })
   const [error, setError] = useState<Error>()
   const [createWorkout, {isLoading: isCreating}] = useCreateWorkoutMutation()
   const [updateWorkout, {isLoading: isUpdating}] = useUpdateWorkoutMutation()
@@ -44,17 +47,22 @@ export function CalendarForm({
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault()
 
-    try {
-      if (workout.workoutId) {
-        await updateWorkout(workout).unwrap()
-      } else {
-        await createWorkout(workout).unwrap()
-      }
+    if (workout.selectedDays.size === 1 && workout.weeksToRepeat === 0) {
+      try {
+        if (workout.workoutId) {
+          await updateWorkout(workout).unwrap()
+        } else {
+          await createWorkout(workout).unwrap()
+        }
 
-      closeModal(e)
-      resetForm()
-    } catch (error) {
-      setError(error as Error)
+        closeModal(e)
+      } catch (error) {
+        setError(error as Error)
+      }
+    } else {
+      console.log('need logic for repeating days')
+      // Logic to map the repeatingDays numbers to dates
+      // Logic to calculate what date would be next week, and the week after that, etc
     }
   }
 
@@ -71,7 +79,6 @@ export function CalendarForm({
       }).unwrap()
 
       closeModal(e)
-      resetForm()
     } catch (error) {
       setError(error as Error)
     }
@@ -105,44 +112,175 @@ export function CalendarForm({
         className="mt-4 block w-full rounded-lg border bg-white p-3 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300"
         value={workout.name}
       />
-      <fieldset className="mt-4">
-        <legend>Workout Type:</legend>
-        <div>
-          <input
-            type="radio"
-            id={WORKOUT_TYPE.TRAINING}
-            name="type"
-            value={WORKOUT_TYPE.TRAINING}
-            checked={workout.type === WORKOUT_TYPE.TRAINING}
-            className="mr-2"
-            onChange={e =>
-              setWorkout(workout => ({
-                ...workout,
-                type: e.target.value as WORKOUT_TYPE,
-              }))
-            }
-          />
-          <label htmlFor={WORKOUT_TYPE.TRAINING}>Training</label>
-        </div>
+      <div className="flex justify-between">
+        <fieldset className="mt-4">
+          <legend>Workout Type:</legend>
+          <div>
+            <input
+              type="radio"
+              id={WORKOUT_TYPE.TRAINING}
+              name="type"
+              value={WORKOUT_TYPE.TRAINING}
+              checked={workout.type === WORKOUT_TYPE.TRAINING}
+              className="mr-2"
+              onChange={e =>
+                setWorkout(workout => ({
+                  ...workout,
+                  type: e.target.value as WORKOUT_TYPE,
+                }))
+              }
+            />
+            <label htmlFor={WORKOUT_TYPE.TRAINING}>Training</label>
+          </div>
 
-        <div>
-          <input
-            type="radio"
-            id={WORKOUT_TYPE.APPOINTMENT}
-            name="type"
-            checked={workout.type === WORKOUT_TYPE.APPOINTMENT}
-            value={WORKOUT_TYPE.APPOINTMENT}
-            className="mr-2"
-            onChange={e =>
-              setWorkout(workout => ({
-                ...workout,
-                type: e.target.value as WORKOUT_TYPE,
-              }))
-            }
-          />
-          <label htmlFor={WORKOUT_TYPE.APPOINTMENT}>Appointment</label>
+          <div>
+            <input
+              type="radio"
+              id={WORKOUT_TYPE.APPOINTMENT}
+              name="type"
+              checked={workout.type === WORKOUT_TYPE.APPOINTMENT}
+              value={WORKOUT_TYPE.APPOINTMENT}
+              className="mr-2"
+              onChange={e =>
+                setWorkout(workout => ({
+                  ...workout,
+                  type: e.target.value as WORKOUT_TYPE,
+                }))
+              }
+            />
+            <label htmlFor={WORKOUT_TYPE.APPOINTMENT}>Appointment</label>
+          </div>
+        </fieldset>
+
+        <div className="flex flex-col">
+          <div className="mt-4 flex divide-x overflow-hidden rounded-lg border bg-white rtl:flex-row-reverse dark:divide-gray-700 dark:border-gray-700 dark:bg-gray-900">
+            <button
+              type="button"
+              onClick={() => toggleDay(1)}
+              className={clsx(
+                'px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-base',
+                {
+                  'bg-blue-300 hover:bg-blue-100': workout.selectedDays.has(1),
+                  'hover:bg-gray-100': !workout.selectedDays.has(1),
+                },
+              )}
+            >
+              M
+            </button>
+
+            <button
+              type="button"
+              onClick={() => toggleDay(2)}
+              className={clsx(
+                'px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-base',
+                {
+                  'bg-blue-300 hover:bg-blue-100': workout.selectedDays.has(2),
+                  'hover:bg-gray-100': !workout.selectedDays.has(2),
+                },
+              )}
+            >
+              T
+            </button>
+
+            <button
+              type="button"
+              onClick={() => toggleDay(3)}
+              className={clsx(
+                'px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-base',
+                {
+                  'bg-blue-300 hover:bg-blue-100': workout.selectedDays.has(3),
+                  'hover:bg-gray-100': !workout.selectedDays.has(3),
+                },
+              )}
+            >
+              W
+            </button>
+
+            <button
+              type="button"
+              onClick={() => toggleDay(4)}
+              className={clsx(
+                'px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-base',
+                {
+                  'bg-blue-300 hover:bg-blue-100': workout.selectedDays.has(4),
+                  'hover:bg-gray-100': !workout.selectedDays.has(4),
+                },
+              )}
+            >
+              T
+            </button>
+
+            <button
+              type="button"
+              onClick={() => toggleDay(5)}
+              className={clsx(
+                'px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-base',
+                {
+                  'bg-blue-300 hover:bg-blue-100': workout.selectedDays.has(5),
+                  'hover:bg-gray-100': !workout.selectedDays.has(5),
+                },
+              )}
+            >
+              F
+            </button>
+
+            <button
+              type="button"
+              onClick={() => toggleDay(6)}
+              className={clsx(
+                'px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-base',
+                {
+                  'bg-blue-300 hover:bg-blue-100': workout.selectedDays.has(6),
+                  'hover:bg-gray-100': !workout.selectedDays.has(6),
+                },
+              )}
+            >
+              S
+            </button>
+
+            <button
+              type="button"
+              onClick={() => toggleDay(0)}
+              className={clsx(
+                'px-4 py-2 text-sm font-medium text-gray-600 transition-colors duration-200 dark:text-gray-300 dark:hover:bg-gray-800 sm:text-base',
+                {
+                  'bg-blue-300 hover:bg-blue-100': workout.selectedDays.has(0),
+                  'hover:bg-gray-100': !workout.selectedDays.has(0),
+                },
+              )}
+            >
+              S
+            </button>
+          </div>
+
+          <div className="mt-2">
+            Repeat for{' '}
+            <input
+              className="w-10"
+              type="number"
+              min="0"
+              value={workout.weeksToRepeat}
+              onChange={e =>
+                setWorkout(workout => ({
+                  ...workout,
+                  weeksToRepeat: Number(e.target.value),
+                }))
+              }
+            />{' '}
+            {workout.weeksToRepeat === 1 ? 'week.' : 'weeks.'}{' '}
+            <div className="text-sm">
+              {workout.weeksToRepeat === 0
+                ? "(Don't repeat; this week only.)"
+                : workout.weeksToRepeat === 1
+                ? '(This week and next week.)'
+                : `(${
+                    workout.weeksToRepeat + 1
+                  } weeks total starting this week.)`}
+            </div>
+          </div>
         </div>
-      </fieldset>
+      </div>
+
       <textarea
         onChange={e =>
           setWorkout(workout => ({
