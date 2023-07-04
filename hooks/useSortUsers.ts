e1d72bea28f80cf0c6d37c596db668a58bd9c198
James <james.d.langridge@gmail.com>
2023-07-04 22:08:08 +0200

    Improve user sorting

---
+++ hooks/useSortUsers.ts
@@ -0,0 +1,40 @@
+'use client'
+
+import {useEffect, useRef, useState} from 'react'
+
+import {SerialisedUser, SerialisedUserKey} from '@/@types/types'
+import {sortUsers} from '@/lib/users'
+import {useGetUsersQuery} from '@/redux/apiSlice'
+
+export function useSortUsers() {
+  const [sortCol, setSortCol] = useState<Record<string, SerialisedUserKey>>({
+    key: 'name',
+  })
+  const [sortedUsers, setSortedUsers] = useState<SerialisedUser[] | undefined>()
+  const {data: users} = useGetUsersQuery()
+  const prevSortColRef = useRef<Record<string, SerialisedUserKey>>()
+  const sortOrder = useRef<'asc' | 'des'>('asc')
+
+  useEffect(() => {
+    if (!users) {
+      return
+    }
+
+    if (prevSortColRef.current?.key === sortCol.key) {
+      sortOrder.current = sortOrder.current === 'asc' ? 'des' : 'asc'
+    } else {
+      sortOrder.current = 'asc'
+    }
+
+    const sorted = sortUsers(
+      sortCol.key,
+      [...users],
+      sortOrder.current === 'asc',
+    )
+
+    setSortedUsers(sorted)
+    prevSortColRef.current = sortCol
+  }, [sortCol, users])
+
+  return {sortedUsers, setSortCol}
+}
