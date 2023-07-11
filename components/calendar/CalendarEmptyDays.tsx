58a06dbfe677523a3c488f90c67e3e12f314d935
James <james.d.langridge@gmail.com>
2023-07-11 22:17:12 +0200

    Remove comment

--- components/calendar/CalendarEmptyDays.tsx
+++ components/calendar/CalendarEmptyDays.tsx
@@ -12,7 +12,6 @@ export function CalendarEmptyDays({emptyDays}: {emptyDays: null[]}) {
       {emptyDays.map((_day, i) => {
         return (
           <div className="border text-center ring-1 ring-gray-400/25" key={i}>
-            {/*TODO: localise empty day names?*/}
             <div className="text-xs lg:text-base">{dayNames[i]}</div>
           </div>
         )
