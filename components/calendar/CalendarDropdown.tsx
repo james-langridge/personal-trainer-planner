994224c150f6492d2035209e07d95cc78aef1f52
James <james.d.langridge@gmail.com>
2023-07-17 16:27:10 +0200

    Add tooltip to dropdown select

--- components/calendar/CalendarDropdown.tsx
+++ components/calendar/CalendarDropdown.tsx
@@ -22,7 +22,11 @@ export function CalendarDropdown() {
   }
 
   return (
-    <Menu as="div" className="relative mt-4 inline-block w-1/5 text-left">
+    <Menu
+      as="div"
+      className="relative inline-block text-left"
+      title="Select a client"
+    >
       <div>
         <Menu.Button className="text-md inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 font-semibold capitalize text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           {user?.name}
