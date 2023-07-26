c234efbd0d3f5150dcd18360f0e0c935aea4ac55
James <james.d.langridge@gmail.com>
2023-07-26 21:12:36 +0200

    Refactor client select dropdown

--- features/calendar/desktop/ClientDropdown.tsx
+++ features/calendar/desktop/ClientDropdown.tsx
@@ -1,70 +1,39 @@
-import {Menu, Transition} from '@headlessui/react'
-import {ChevronDownIcon} from '@heroicons/react/20/solid'
-import clsx from 'clsx'
-import React, {Fragment} from 'react'
-
 import {UserWithWorkouts} from '@/@types/apiResponseTypes'
-import {useAppDispatch, useAppSelector} from '@/redux/hooks'
-import {selectUser, setUser} from '@/redux/usersSlice'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/select'
+import {useAppDispatch} from '@/redux/hooks'
+import {setUser} from '@/redux/usersSlice'
 
 export function ClientDropdown({users}: {users: UserWithWorkouts[]}) {
-  const user = useAppSelector(selectUser)
   const dispatch = useAppDispatch()
 
-  const onCLick = (user: UserWithWorkouts) => {
-    dispatch(setUser(user))
+  const onSelect = (userName: string) => {
+    dispatch(setUser(users.find(user => user.name === userName)))
   }
 
   return (
-    <Menu
-      as="div"
-      className="relative mx-1 inline-block text-left"
-      title="Select a client"
-    >
-      <div>
-        <Menu.Button className="text-md inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 font-semibold capitalize text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-          {user?.name}
-          <ChevronDownIcon
-            className="-mr-1 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-        </Menu.Button>
-      </div>
-
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="w-50 absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
-            {users.map(user => {
-              return (
-                <Menu.Item key={user.id}>
-                  {({active}) => (
-                    <button
-                      onClick={() => onCLick(user)}
-                      className={clsx(
-                        {
-                          'bg-gray-100 text-gray-900': active,
-                          'text-gray-700': !active,
-                        },
-                        'text-md block w-44 px-4 py-2 capitalize',
-                      )}
-                    >
-                      {user.name}
-                    </button>
-                  )}
-                </Menu.Item>
-              )
-            })}
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
+    <Select onValueChange={userName => onSelect(userName)}>
+      <SelectTrigger className="w-[180px] capitalize">
+        <SelectValue placeholder="Select a client" />
+      </SelectTrigger>
+      <SelectContent>
+        {users.map(user => {
+          return (
+            <SelectItem
+              key={user.id}
+              value={user.name || ''}
+              className="capitalize"
+            >
+              {user.name}
+            </SelectItem>
+          )
+        })}
+      </SelectContent>
+    </Select>
   )
 }
--- features/calendar/desktop/ClientSelect.tsx
+++ features/calendar/desktop/ClientSelect.tsx
@@ -1,5 +1,5 @@
 import {USER_TYPE} from '@prisma/client'
-import React, {useEffect, useState} from 'react'
+import {useState} from 'react'
 
 import {AddButton} from '@/components/AddButton'
 import {ClientDropdown} from '@/features/calendar/desktop/ClientDropdown'
@@ -10,9 +10,9 @@ import {useGetUsersQuery} from '@/redux/apiSlice'
 export function ClientSelect() {
   const [clientType, setClientType] = useState<USER_TYPE>(USER_TYPE.INDIVIDUAL)
   const {data = []} = useGetUsersQuery()
+  const filteredUsers = data.filter(user => user.type === clientType)
   // Case-insensitive sorting is not possible via a Prisma query
   // https://www.prisma.io/docs/concepts/components/prisma-client/filtering-and-sorting#can-i-perform-case-insensitive-sorting
-  const filteredUsers = data.filter(user => user.type === clientType)
   const users = sortByString('name', filteredUsers)
 
   function toggleClientType() {
