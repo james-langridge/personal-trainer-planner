944c746a77a9b34a662987a1d602435537e2074b
James <james.d.langridge@gmail.com>
2023-07-27 10:04:05 +0200

    Update event specific links

--- features/calendar/bootcamp/BootcampItemMobile.tsx
+++ features/calendar/bootcamp/BootcampItemMobile.tsx
@@ -16,7 +16,7 @@ export function BootcampItemMobile({bootcamp}: {bootcamp: Bootcamp}) {
       />
 
       <Link
-        href={`/workout/${bootcamp?.id}`}
+        href={`/bootcamp/${bootcamp?.id}`}
         className="my-1 block w-full rounded bg-yellow-400 px-2 text-white"
       >
         {bootcamp?.name}
