25fb2ac80f0b78feafa42a3202ab78ec36a80add
James <james.d.langridge@gmail.com>
2023-07-27 09:36:22 +0200

    Create appointment and bootcamp pages

--- app/appointment/[slug]/page.tsx
+++ app/appointment/[slug]/page.tsx
@@ -2,19 +2,19 @@
 
 import BackButton from '@/components/BackButton'
 import {useScrollToTop} from '@/hooks'
-import {useGetWorkoutQuery} from '@/redux/apiSlice'
+import {useGetAppointmentQuery} from '@/redux/apiSlice'
 
-export default function Workout({params}: {params: {slug: string}}) {
+export default function Appointment({params}: {params: {slug: string}}) {
   const {slug} = params
-  const {data: workout} = useGetWorkoutQuery(slug)
+  const {data: appointment} = useGetAppointmentQuery(slug)
   // TODO: Temp fix for page opening scrolled to bottom on mobile view
   useScrollToTop()
 
-  if (!workout) {
+  if (!appointment) {
     return null
   }
 
-  const date = new Date(workout.date).toLocaleString('default', {
+  const date = new Date(appointment.date).toLocaleString('default', {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
@@ -25,13 +25,13 @@ export default function Workout({params}: {params: {slug: string}}) {
     <div className="flex justify-center p-10">
       <div className="prose w-screen text-center">
         <h1>{date}</h1>
-        <h2 className="capitalize">{workout.name}</h2>
-        <p>{workout.description}</p>
-        {workout.videoUrl && (
+        <h2 className="capitalize">{appointment.name}</h2>
+        <p>{appointment.description}</p>
+        {appointment.videoUrl && (
           <iframe
             title="Fit For Life Trainer Intro"
             className="mt-12 h-64 min-w-full overflow-hidden rounded-xl border-none md:h-[450px]"
-            src={workout.videoUrl}
+            src={appointment.videoUrl}
             allow="autoplay; fullscreen"
             allowFullScreen={false}
           ></iframe>
