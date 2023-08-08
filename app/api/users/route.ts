f1a7148ed4e66517270189755671e63485b36cde
James <james.d.langridge@gmail.com>
2023-08-08 14:29:40 +0200

    Create API to update user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,7 @@
 import {NextRequest, NextResponse} from 'next/server'
 import {getServerSession} from 'next-auth/next'
 
-import {CreateUserBody} from '@/@types/apiRequestTypes'
+import {CreateUserBody, UpdateUserBody} from '@/@types/apiRequestTypes'
 import {UserWithWorkouts} from '@/@types/apiResponseTypes'
 import {authOptions} from '@/app/api/auth/[...nextauth]/route'
 import {db} from '@/lib/db'
@@ -116,6 +116,38 @@ export async function POST(req: NextRequest) {
       email: body.email,
       name: body.name,
       type: body.type,
+    },
+  })
+
+  return NextResponse.json(
+    {user},
+    {
+      status: 201,
+    },
+  )
+}
+
+export async function PUT(req: NextRequest) {
+  const session = await getServerSession(authOptions)
+
+  if (!session) {
+    return NextResponse.json({message: 'You must be logged in.'}, {status: 401})
+  }
+
+  if (session.user?.role !== 'admin') {
+    return NextResponse.json({message: 'Forbidden.'}, {status: 403})
+  }
+
+  const body: UpdateUserBody = await req.json()
+
+  const user = await db.appointment.update({
+    where: {
+      id: body.id,
+    },
+    data: {
+      ...(body.email && {email: body.email}),
+      ...(body.name && {name: body.name}),
+      ...(body.type && {type: body.type}),
     },
   })
 
