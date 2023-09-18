cf40e97b591f36e7e9dd653238dc6e0f768dd900
James <james.d.langridge@gmail.com>
2023-09-18 13:09:07 +0200

    Give admin mobile-optimised calendar view

--- features/calendar/Calendar.tsx
+++ features/calendar/Calendar.tsx
@@ -1,21 +1,18 @@
 import {CalendarDesktop} from '@/features/calendar/desktop'
 import {CalendarMobile} from '@/features/calendar/mobile'
 import {useIsMobile} from '@/hooks'
-import {selectIsAdmin} from '@/redux/authSlice'
-import {useAppSelector} from '@/redux/store'
 
 import {useUser} from '.'
 
 export function Calendar({initialUser}: {initialUser: string}) {
   useUser(JSON.parse(initialUser))
   const isMobile = useIsMobile()
-  const isAdmin = useAppSelector(selectIsAdmin)
 
   return (
     <div className="flex h-[90vh]">
       <div className="flex w-full flex-col px-5 sm:items-center ">
-        {isMobile && !isAdmin && <CalendarMobile />}
-        {(!isMobile || isAdmin) && <CalendarDesktop />}
+        {isMobile && <CalendarMobile />}
+        {!isMobile && <CalendarDesktop />}
       </div>
     </div>
   )
