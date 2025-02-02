87e8292170fdc913ae0d5fd75bb406e72dcd3acb
James Langridge <james.langridge@orbitlabs.de>
2025-02-02 21:50:28 +0100

    Lazy load main calendar components

--- features/calendar/desktop/CalendarDesktop.tsx
+++ features/calendar/desktop/CalendarDesktop.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import {Grid, Header, useCalendarData} from '.'
 
-export function CalendarDesktop() {
+export default function CalendarDesktop() {
   const {monthData, year, month, setYear, setMonth} = useCalendarData()
 
   return (
