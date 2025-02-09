184ccb6de4b606afb18490c686874f37ed84385c
James Langridge <l@ngridge.com>
2025-02-09 23:19:38 +0100

    Delete unused hooks

--- hooks/index.ts
+++
@@ -1,7 +0,0 @@
-'use client'
-
-export * from './useIntersectionObserver'
-export * from './useIsFirstRender'
-export * from './useIsMobile'
-export * from './useMediaQuery'
-export * from './useScrollToTop'
