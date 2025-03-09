738f3c8f682615ad76628146851f99562b00b98c
James Langridge <l@ngridge.com>
2025-03-09 22:37:22 +0100

    Improve e2e testing with CI database

--- e2e/e2e.spec.ts
+++ e2e/e2e.spec.ts
@@ -1,21 +1,33 @@
-import {test} from '@playwright/test'
+import {test, expect} from '@playwright/test'
 
 test('test', async ({page}) => {
+  const now = new Date()
+  const currentYear = now.getFullYear()
+  const currentMonth = now.getMonth()
+
+  const bootcampDay7 = `${currentYear}-${currentMonth}-7`
+  const workoutDay15 = `${currentYear}-${currentMonth}-15`
+  const bootcampCheckbox = page.getByTestId(bootcampDay7).getByRole('checkbox')
+  const workoutCheckbox = page.getByTestId(workoutDay15).getByRole('checkbox')
+
   await page.goto('/auth/signin')
   await page.getByRole('textbox', {name: 'Email address'}).click()
   await page
     .getByRole('textbox', {name: 'Email address'})
-    .fill('james.d.langridge@gmail.com')
+    .fill('test@example.com')
   await page.getByRole('textbox', {name: 'Password'}).click()
-  await page
-    .getByRole('textbox', {name: 'Password'})
-    .fill('BOWLDER1wood.eternal')
+  await page.getByRole('textbox', {name: 'Password'}).fill('test123')
   await page.getByRole('button', {name: 'Sign in'}).click()
 
-  await page.getByTestId('prevMonthBtn').click()
-  await page.getByTestId('2025-1-7').getByRole('checkbox').check()
-  await page.getByTestId('2025-1-14').getByRole('checkbox').uncheck()
-  await page.getByTestId('2025-1-21').getByRole('checkbox').check()
-  await page.getByTestId('2025-1-21').getByRole('checkbox').uncheck()
-  await page.getByTestId('2025-1-6').getByRole('checkbox').check()
+  await bootcampCheckbox.check()
+  await expect(bootcampCheckbox).toBeChecked()
+  await expect(bootcampCheckbox).toBeEnabled({timeout: 10_000})
+  await bootcampCheckbox.uncheck()
+  await expect(bootcampCheckbox).not.toBeChecked()
+
+  await workoutCheckbox.check()
+  await expect(workoutCheckbox).toBeChecked()
+  await expect(workoutCheckbox).toBeEnabled({timeout: 10_000})
+  await workoutCheckbox.uncheck()
+  await expect(workoutCheckbox).not.toBeChecked()
 })
