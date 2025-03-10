e184b7be1ed1d9d7e0cd6fadc137f8ba403471b4
James Langridge <l@ngridge.com>
2025-03-10 22:47:09 +0100

    Add playwright debugging

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'github' : 'list',
+  reporter: [['html'], ['list', {printSteps: true}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
