fbe6b2ddee956330ef50a0fbb90b104a298d2a59
James Langridge <l@ngridge.com>
2025-03-14 11:27:35 +0100

    Add MS Edge to playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -57,6 +57,10 @@ export default defineConfig({
       name: 'Mobile Safari',
       use: {...devices['iPhone 12']},
     },
+    {
+      name: 'Microsoft Edge',
+      use: {...devices['Desktop Edge'], channel: 'msedge'},
+    },
     // {
     //   name: 'Google Chrome',
     //   use: {...devices['Desktop Chrome'], channel: 'chrome'},
