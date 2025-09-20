8e5c7e5ae650f76f0c7bb06de0a000616827deaa
James Langridge <l@ngridge.com>
2025-09-20 21:59:11 +0200

    Fix import order linting warning
    - Add blank line between import groups in admin calendar page

--- app/admin/calendar/page.tsx
+++ app/admin/calendar/page.tsx
@@ -1,4 +1,5 @@
 import {headers} from 'next/headers'
+
 import {CalendarDesktop} from '@/features/calendar/desktop'
 import {CalendarMobileAdmin} from '@/features/calendar/mobile/CalendarMobileAdmin'
 import {isMobileViewport} from '@/lib/utils'
